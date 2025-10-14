fc560efd2e903a7d023f72be4ddf7d39ce03c1ca
thillard2 <thomasaurus12@gmail.com>
2025-10-14 13:28:45 -0500

    Added Time bins and started setting up code for co occur analysis with cooccur package

--- Scripts/FinalProject.R
+++ Scripts/FinalProject.R
@@ -1,5 +1,5 @@
 #Reading data csv
-CoMatrix=read.csv("Complete PAtable_new.csv")
+CoMatrix=read.csv("data/Complete PAtable_new.csv")
 #Loading needed packages
 #I had to manually install the cooccur package that I need as it wasn't on CRAN
 #I was able to download it manually as a .tar file and install it manually
@@ -28,8 +28,16 @@ Comatrixnoyearint= Comatrixnoyear %>% mutate_all(as.integer)
 bruhswouse=cooccur(Comatrixnoyearint, type="spp_site",thresh=TRUE,spp_names=TRUE)
 summary(bruhswouse6)
 
-Timebin1=CoMatrixFix[,CoMatrixFix[1,]>=30000]
-Timebin2=CoMatrixFix[,CoMatrixFix[1,]<=30000 & CoMatrixFix[1,]>=20000]
-Timebin2noyear=Timebin2[-1,]
-Timebin2noyearint=Timebin2 %>% mutate_all(as.integer)
-bruhswouse1=cooccur(Timebin2noyearint, type="spp_site",thresh=TRUE, spp_names=TRUE)
+Timebin1=CoMatrixFix[,CoMatrixFix[1,]<=30000 & CoMatrixFix[1,]>=25000]
+Timebin2=CoMatrixFix[,CoMatrixFix[1,]<25000 & CoMatrixFix[1,]>=20000]
+Timebin3=CoMatrixFix[,CoMatrixFix[1,]<20000 & CoMatrixFix[1,]>=15000]
+Timebin4=CoMatrixFix[,CoMatrixFix[1,]<15000 & CoMatrixFix[1,]>=10000]
+Timebin5=CoMatrixFix[,CoMatrixFix[1,]<10000 & CoMatrixFix[1,]>=5000]
+Timebin6=CoMatrixFix[,CoMatrixFix[1,]<5000 & CoMatrixFix[1,]>=0]
+Timebin1noyear=Timebin1[-1,]
+Timebin1noyearint=Timebin1noyear %>% mutate_all(as.integer)
+bruhswouse1=cooccur(Timebin1noyearint, type="spp_site",thresh=TRUE, spp_names=TRUE)
+
+coocfun= lapply(list())
+Timebin1ProportionSig=(bruhswouse1$positive+bruhswouse1$negative)/(bruhswouse1$positive+bruhswouse1$negative+bruhswouse1$random)
+Timebin1ProportionAgg=(bruhswouse1$positive)/(bruhswouse1$positive+bruhswouse1$negative)
