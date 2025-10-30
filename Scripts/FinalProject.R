57c9a2e7b4bc0bb939092d9215f2cfea2b901501
thillard2 <thomasaurus12@gmail.com>
2025-10-30 12:35:37 -0500

    Cleaned up Code, added code for the plots and converted the lists output from the cooccur analysis into a format that ggplot can use

--- Scripts/FinalProject.R
+++ Scripts/FinalProject.R
@@ -47,9 +47,38 @@ CoocTime6=coocfun[[6]]
 #Putting the results from the co-occur analysis in list. Special object type called cooccur
 Cooctotals=list(CoocTime1,CoocTime2,CoocTime3,CoocTime4,CoocTime5,CoocTime6)
 #Calculating the proportion of significant pairs by dividing the sum of positive and negative pairs for each time bin by total pairs
-TotalProportionsSig=lapply(Cooctotals, FUN=function(x)(x$positive+x$negative)/(x$positive+x$negative+x$random))
+TotalProportionsSig=lapply(Cooctotals, FUN=function(x)(x$co_occurrences)/(x$pairs))
 #Calculating the proportion of aggregate pairs by dividing the number of aggregate pairs by the sum of significant species pairs in each time bin
-TotalProportionsAgg=lapply(Cooctotals,FUN=function(x) (x$positive)/(x$positive+x$negative))
+TotalProportionsAgg=lapply(Cooctotals,FUN=function(x) (x$positive)/(x$co_occurrences))
 
-Timebin1ProportionSig=(bruhswouse1$positive+bruhswouse1$negative)/(bruhswouse1$positive+bruhswouse1$negative+bruhswouse1$random)
-Timebin1ProportionAgg=(bruhswouse1$positive)/(bruhswouse1$positive+bruhswouse1$negative)
+#Turning the list of proportions into a dataframe for ggplot
+TotalProportionsSig=as.data.frame(TotalProportionsSig)
+#Adding a row to the dataframe for the categorical timebins for ggplot, added timebin labels
+TotalProportionsSig[nrow(TotalProportionsSig)+1,]=c("Timebin1","Timebin2","Timebin3","Timebin4","Timebin5","Timebin6")
+#Transposing the dataframe so that the values for proportions are a column not a row
+TotalProportionsSig=data.frame(t(TotalProportionsSig))
+#Turned the proportion values to numerics from characters
+TotalProportionsSig$X1=as.numeric(TotalProportionsSig$X1)
+#Plot for proportion of significant pairs. stat="identity for single values
+#Used cowplot theme, added labels and title. Changed element text to make it look better
+ggplot(TotalProportionsSig, aes(x=X2,y=X1))+geom_bar(stat="identity", fill="skyblue", width=0.5)+
+  theme_cowplot()+labs(x="Time",y="Proportion of Nonrandom Pairs",title="Proportion of Nonrandom Pairs Through Time in North America")+
+  theme(plot.title=element_text(size=12), axis.title.y=element_text(size=10),
+        axis.title.x=element_blank(), axis.text.y=element_text(size=10))
+
+#Turning the list of proportions into a dataframe for ggplot
+TotalProportionsAgg=as.data.frame(TotalProportionsAgg)
+#Adding a row to the dataframe for the categorical timebins for ggplot, added timebin labels
+TotalProportionsAgg[nrow(TotalProportionsAgg)+1,]=c("Timebin1","Timebin2","Timebin3","Timebin4","Timebin5","Timebin6")
+#Transposing the dataframe so that the values for proportions are a column not a row
+TotalProportionsAgg=data.frame(t(TotalProportionsAgg))
+#Turned the proportion values to numerics from characters
+TotalProportionsAgg$X1=as.numeric(TotalProportionsAgg$X1)
+#Plot for proportion of aggregated pairs. stat="identity for single values
+#Used cowplot theme, added labels and title. Changed element text to make it look better, also changed axis range to illuminate trend
+ggplot(TotalProportionsAgg, aes(x=X2,y=X1))+geom_bar(stat="identity", fill="tomato", width=0.5)+
+  theme_cowplot()+labs(x="Time",y="Proportion of Aggregated Pairs", title="Proportion of Aggregated Pairs Through Time in North America")+
+  theme(plot.title=element_text(size=12), axis.title.y=element_text(size=10),
+          axis.title.x=element_blank(), axis.text.y=element_text(size=10))+
+  coord_cartesian(ylim=c(0.4,0.9))
+  
